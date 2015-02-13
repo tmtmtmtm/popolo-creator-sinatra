8f6770c0bdc1aa58efb10ce637b66f72cf83fbc9
Tony Bowden <tony@tmtm.com>
2015-02-13 14:21:47 +0000

    Add a /convert endpoint

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require 'colorize'
+require 'csv_to_popolo'
 
 #---------------------------------------------------------------------
 # Static pages
@@ -11,6 +12,10 @@ get '/' do
   haml :index
 end
 
+post '/convert' do
+  file = params[:csv][:tempfile]
+  JSON.pretty_generate(Popolo::CSV.from_file(file).data)
+end
 
 helpers do
 
