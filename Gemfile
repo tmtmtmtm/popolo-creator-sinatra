8dd2200b5580946c6f7143d3ece7b166008b0b60
Tony Bowden <tony@tmtm.com>
2015-02-19 14:29:35 +0000

    Add CORS headers

--- Gemfile
+++ Gemfile
@@ -7,4 +7,6 @@ gem 'thin'
 gem 'redcarpet'
 gem 'open-uri-cached'
 gem 'colorize'
+gem "sinatra-cross_origin", "~> 0.3.1"
 gem 'csv_to_popolo', '>=0.4.0', :github => 'tmtmtmtm/csv_to_popolo'
+
--- app.rb
+++ app.rb
@@ -2,12 +2,15 @@ require 'sinatra'
 require 'haml'
 require 'json'
 require 'csv_to_popolo'
+require 'sinatra/cross_origin'
+
 
 get '/' do
   haml :index
 end
 
 post '/convert' do
+  cross_origin
   content_type :json
   begin
     file = params[:csv][:tempfile] 
